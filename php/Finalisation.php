8b7f4e3c8c38a816fa102fed6bc556921b9a192a
GuillaumeB13 <barthelemy.guillaume1@hotmail.fr>
2016-02-22 22:34:13 +0100

    légère modif

--- php/Finalisation.php
+++ php/Finalisation.php
@@ -24,32 +24,7 @@
 				</center>
 				<section>
 					<form method="."post"." class=\"form-group\"><br><br>";
-						// requete php pour recupérer nom des type de champs à récupérer en BDD (correspond aux noeuds dans le doc xml)
-						$req=$PDO_BDD->query('SELECT type_Champs FROM `Champs` WHERE id_Masks="'.$_SESSION['type_masque'].'"')->fetchAll();
-						// stockages des types dans le tableau
-						$xml = simplexml_load_file('SavedData.xml'); // ouverture du XML
-
-						$var=array();
-						$champs=array();
-
-						foreach($req as $ligne)
-						{
-							$var[] = $xml->$ligne['type_Champs'];
-							$champs[] = $ligne['type_Champs'];
-						}
-						
-						for($i=0;$i<sizeof($var);$i++)
-						{
-							/*if ($champs[$i]=="PhotoID" || $champs[$i]=="Signature")
-							{
-								$data=base64_decode($var[$i]);
-								$img=imagecreatefromstring($data);
-								imagepng($img);
-
-							}
-							else*/
-								echo "<label class=\"col-sm-offset-1\">$champs[$i]</label>".' '.'<input type="text" value="'.$var[$i].'" name="'.$champs[$i].'""><br>';
-						}
+					include_once 'data.php';
 					echo "
 					<center>
 						<h2>Enregistrer les données récupérées ?</h2>
@@ -136,4 +111,5 @@
 					color: red;
 				}
 				</style>";
+
 ?>
---
+++ php/data.php
@@ -0,0 +1,30 @@
+<?php
+	include_once 'config.php';
+	// requete php pour recupérer nom des type de champs à récupérer en BDD (correspond aux noeuds dans le doc xml)
+	$req=$PDO_BDD->query('SELECT type_Champs FROM `Champs` WHERE id_Masks="'.$_SESSION['type_masque'].'"')->fetchAll();
+	// stockages des types dans le tableau
+	$xml = simplexml_load_file('SavedData.xml'); // ouverture du XML
+
+	$var=array();
+	$champs=array();
+
+	foreach($req as $ligne)
+	{
+		$var[] = $xml->$ligne['type_Champs'];
+		$champs[] = $ligne['type_Champs'];
+	}
+
+	for($i=0;$i<sizeof($var);$i++)
+	{
+		/*if ( $champs[$i]=='Signature' || $champs[$i]=='PhotoID')
+		{
+			$data=base64_decode($var[$i]);
+			$img=imagecreatefromstring($data);
+			//header('Content-Type: image/png');
+			imagepng($img);
+
+		}
+		else*/
+			echo "<label class=\"col-sm-offset-1\">$champs[$i]</label>".' '.'<input type="text" value="'.$var[$i].'" name="'.$champs[$i].'""><br>';
+	}
+?>
